24175394d66c967e0d816a6abbc8932da951a00b
Szymon <szymonnie2891@gmail.com>
2024-09-11 20:19:46 +0200

    First commit

---
+++ ConvertCurrencyWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjektWłasny
+{
+    public partial class ConvertCurrencyWindow : Window
+    {
+        private List<CurrencyRate> _currencyRates;
+
+        public ConvertCurrencyWindow(List<CurrencyRate> currencyRates)
+        {
+            InitializeComponent();
+            _currencyRates = currencyRates;
+            LoadCurrencies();
+        }
+
+        private void LoadCurrencies()
+        {
+            var currencies1 = _currencyRates.Select(cr => cr.FromCurrency).Distinct().ToList();
+            var currencies2 = _currencyRates.Select(cr => cr.ToCurrency).Distinct().ToList();
+            FromCurrencyComboBox.ItemsSource = currencies1;
+            ToCurrencyComboBox.ItemsSource = currencies2;
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(AmountTextBox.Text, out double amount))
+            {
+                string fromCurrency = FromCurrencyComboBox.SelectedItem as string;
+                string toCurrency = ToCurrencyComboBox.SelectedItem as string;
+
+                var rate = _currencyRates.FirstOrDefault(cr => cr.FromCurrency == fromCurrency && cr.ToCurrency == toCurrency)?.Rate;
+
+                if (rate != null)
+                {
+                    double convertedAmount = amount * rate.Value;
+                    ResultTextBlock.Text = $"Converted Amount: {convertedAmount:F2} {toCurrency}";
+                }
+                else
+                {
+                    MessageBox.Show("Conversion rate not found");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid amount");
+            }
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjektWłasny
+{
+    public partial class MainWindow : Window
+    {
+        public List<CurrencyRate> CurrencyRates { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            CurrencyRates = new List<CurrencyRate>
+            {
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "EUR", Rate = 0.85 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "GBP", Rate = 0.75 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "PLN", Rate = 4.03 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "YEN", Rate = 1232.45 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "LVY", Rate = 2.34 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "RUB", Rate = 343.45 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "CSK", Rate = 342.56 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "KUN", Rate = 1.43 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "TBT", Rate = 20392.34 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "MON", Rate = 34.56 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "CPS", Rate = 233.34 },
+                new CurrencyRate { FromCurrency = "USD", ToCurrency = "DIN", Rate = 39.48 }
+            };
+
+            CurrencyListView.ItemsSource = CurrencyRates;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ConvertCurrency_Click(object sender, RoutedEventArgs e)
+        {
+            ConvertCurrencyWindow convertCurrencyWindow = new ConvertCurrencyWindow(CurrencyRates);
+            convertCurrencyWindow.ShowDialog();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow aboutWindow = new AboutWindow();
+            aboutWindow.ShowDialog();
+        }
+    }
+
+    public class CurrencyRate
+    {
+        public string FromCurrency { get; set; }
+        public string ToCurrency { get; set; }
+        public double Rate { get; set; }
+    }
+}
